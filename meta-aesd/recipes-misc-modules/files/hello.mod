efe3fd60f43f5e2d025b00bc3ebd515bb3c03e4d
jainaneesh <jainaneesh15@gmail.com>
2025-05-29 11:52:17 -0400

    commiting everything before closing today on this

---
+++ meta-aesd/recipes-misc-modules/files/hello.mod
@@ -0,0 +1,2 @@
+/home/linux-o-phile/assignment-6-jainaneesh/meta-aesd/recipes-misc-modules/files/hello.o
+
---
+++ meta-aesd/recipes-misc-modules/files/proc_ops_version.h
@@ -0,0 +1,19 @@
+// proc_ops_version.h
+#ifndef _PROC_OPS_VERSION_H
+#define _PROC_OPS_VERSION_H
+
+#include <linux/version.h>
+#include <linux/proc_fs.h>
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,6,0)
+#define PROC_OPS struct proc_ops
+#define PROC_READ_FUNC .proc_read
+#define PROC_WRITE_FUNC .proc_write
+#else
+#define PROC_OPS struct file_operations
+#define PROC_READ_FUNC .read
+#define PROC_WRITE_FUNC .write
+#endif
+
+#endif /* _PROC_OPS_VERSION_H */
+
