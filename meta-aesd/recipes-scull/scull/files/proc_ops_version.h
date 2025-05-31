c9d7295dccc6ba593692ee357870fe884ef90bbc
jainaneesh <jainaneesh15@gmail.com>
2025-05-30 22:21:10 -0400

    added additional header files

---
+++ meta-aesd/recipes-scull/scull/files/proc_ops_version.h
@@ -0,0 +1,23 @@
+// proc_ops_version.h
+#ifndef _PROC_OPS_VERSION_H
+#define _PROC_OPS_VERSION_H
+
+#include <linux/version.h>
+#include <linux/proc_fs.h>
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,6,0)
+#define PROC_OPS struct proc_ops
+#define PROC_OPEN .proc_open
+#define PROC_READ .proc_read
+#define PROC_LSEEK .proc_lseek
+#define PROC_RELEASE .proc_release
+#else
+#define PROC_OPS struct file_operations
+#define PROC_OPEN .open
+#define PROC_READ .read
+#define PROC_LSEEK .llseek
+#define PROC_RELEASE .release
+#endif
+
+#endif /* _PROC_OPS_VERSION_H */
+
